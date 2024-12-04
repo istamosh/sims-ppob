d22660c0ff056118bcc8b99a26577b9b68b2eeb7
istamosh <63596969+istamosh@users.noreply.github.com>
2024-12-04 14:57:29 +0700

    update: balance section

--- src/components/UserSection.tsx
+++ src/components/UserSection.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 
 const UserSection = () => {
@@ -8,7 +9,20 @@ const UserSection = () => {
         <p>Selamat datang,</p>
         <p className="font-bold text-2xl">John Wick</p>
       </div>
-      <div className="w-3/5 bg-red-500 rounded-lg"></div>
+      <div className="w-3/5 border border-red-500 relative">
+        <Image
+          src="/assets/Background Saldo.png"
+          alt="bg-saldo"
+          width={670}
+          height={161}
+          className="w-full h-full"
+        />
+        <div className="absolute top-0 left-0 text-white p-4 h-full flex flex-col justify-between">
+          <p>Saldo Anda</p>
+          <p className="font-bold text-2xl">Rp. 0</p>
+          <button className="text-sm">Tutup Saldo</button>
+        </div>
+      </div>
     </div>
   );
 };
